3906a2631f2c5dfff7507fac6e57f4b8cfa421c7
Ruhan <18ruhan@gmail.com>
2024-01-18 19:28:17 +0530

    final

--- coursereg/registration/forms.py
+++ coursereg/registration/forms.py
@@ -6,4 +6,4 @@ from .models import Student
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['first_name', 'last_name', 'email', 'course_name']
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'date_of_birth', 'gender', 'highest_qualification', 'blood_group']
--- coursereg/registration/models.py
+++ coursereg/registration/models.py
@@ -10,7 +10,11 @@ class Student(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     course_name = models.CharField(max_length=100)
-    registration_date = models.DateTimeField(auto_now_add=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    date_of_birth = models.DateField(blank= True,null=True)
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female'), ('other', 'Other')],default='Unknown')
+    highest_qualification = models.CharField(max_length=100,default='Unknown')
+    blood_group = models.CharField(max_length=5, default='Unknown')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
